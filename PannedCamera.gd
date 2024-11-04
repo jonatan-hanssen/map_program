f48efd74fef14854e506463fe939d9b8fb005719
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2024-11-04 16:12:13 +0100

    Fine tune it even more, rename variables

--- PannedCamera.gd
+++ PannedCamera.gd
@@ -7,7 +7,7 @@ const ZOOM_INCREMENT: float = 0.1
 
 var _target_zoom: float = 1
 
-var modHeld : bool = false
+var mod_held : bool = false
 
 
 
@@ -18,19 +18,19 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 
 	if event.is_action_pressed('mod'):
-		modHeld = true
+		mod_held = true
 
 	if event.is_action_released('mod'):
-		modHeld = false
+		mod_held = false
 
 	if event is InputEventMouseMotion:
 
 		if event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
 			position -= event.relative / zoom
-	if event.is_action_pressed('zoom_in') and not modHeld:
+	if event.is_action_pressed('zoom_in') and not mod_held:
 		_target_zoom = min(_target_zoom + ZOOM_INCREMENT, MAX_ZOOM)
 		zoom = Vector2.ONE * _target_zoom
 
-	elif event.is_action_pressed('zoom_out') and not modHeld:
+	elif event.is_action_pressed('zoom_out') and not mod_held:
 		_target_zoom = max(_target_zoom - ZOOM_INCREMENT, MIN_ZOOM)
 		zoom = Vector2.ONE * _target_zoom
--- root.gd
+++ root.gd
@@ -1,47 +1,47 @@
 extends Control
 
 
-@onready var lines: Node2D = $Line2D
-@onready var fileMenu: PopupMenu = $GUI/MenuBar/File
-@onready var fileDialog: FileDialog = $FileDialog
+@onready var file_menu: PopupMenu = $GUI/MenuBar/File
+@onready var settings_menu: PopupMenu = $GUI/MenuBar/Settings
+@onready var file_dialog: FileDialog = $FileDialog
+@onready var save_dialog: FileDialog = $FileDialog2
 @onready var camera: Camera2D = $Camera2D
-@onready var playerWindow: Window = $PlayerWindow
-@onready var fog = $Fog
-@onready var fog2 = $PlayerWindow/Fog
-@onready var backgroundNode = $BackgroundNode
+@onready var player_window: Window = $PlayerWindow
+@onready var dm_fog = $Fog
+@onready var player_fog = $PlayerWindow/Fog
+@onready var background_node = $BackgroundNode
 @onready var background = $BackgroundNode/Background
+
+
+var hovering_over_gui : bool = false
 
 const LightTexture = preload('res://Light.png')
 const DarkTexture = preload('res://Dark.png')
 const GrayTexture = preload('res://Gray.png')
 
-var blendImageHeight : int = 50
-var blendImageWidth : int = 50
+var blend_rect_size : int = 50
 
-var modHeld : bool = false
+var black_circle_bool : bool = false
 
+var mod_held : bool = false
 
+var image : Image
 
-var imgHeight : int
-var imgWidth : int
+var img_height : int
+var img_width : int
 
-var fogImage : Image
-var fogTexture : ImageTexture
+var dm_fog_image : Image
+var dm_fog_texture : ImageTexture
 
-var fogImage2 : Image
-var fogTexture2 : ImageTexture
+var player_fog_image : Image
+var player_fog_texture : ImageTexture
 
-var lightImage : Image
-var darkImage : Image
-var grayImage : Image
+var light_brush : Image
+var dark_brush : Image
+var gray_brush : Image
 
-
-var blendRect : Rect2
-
-var offset : Vector2
-
-var pressed: bool = false
-var pressed2: bool = false
+var m1_pressed: bool = false
+var m2_pressed: bool = false
 var current_line: Line2D = null
 
 
@@ -51,47 +51,51 @@ var COLOR = Color.BLUE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fileMenu.connect('id_pressed', _on_file_id_pressed)
-	fileDialog.add_filter("*.png, *.jpg, *.jpeg", "Images")
-	fileDialog.popup()
-	# playerWindow.world_2d = get_window().world_2d
+	file_menu.connect('id_pressed', _on_file_id_pressed)
+	settings_menu.connect('id_pressed', _on_settings_id_pressed)
+	file_dialog.add_filter("*.png, *.jpg, *.jpeg, *.map", "Images/.map files")
+	save_dialog.add_filter("*.map", ".map files")
+	update_brushes()
+
+	var args = OS.get_cmdline_args()
+
+	if len(args) > 0:
+		load_map(args[0])
+
+	else:
+		file_dialog.popup()
 
 
-	lightImage = LightTexture.get_image()
-	lightImage.resize(blendImageHeight, blendImageWidth)
-	lightImage.convert(Image.FORMAT_RGBAH)
+func update_brushes(value: int = 0) -> void:
+	blend_rect_size = max(5, blend_rect_size + value)
 
-	darkImage = DarkTexture.get_image()
-	darkImage.resize(blendImageHeight, blendImageWidth)
-	darkImage.convert(Image.FORMAT_RGBAH)
+	light_brush = LightTexture.get_image()
+	dark_brush = DarkTexture.get_image()
+	gray_brush = GrayTexture.get_image()
 
-	grayImage = GrayTexture.get_image()
-	grayImage.resize(blendImageHeight, blendImageWidth)
-	grayImage.convert(Image.FORMAT_RGBAH)
+	var imglist = [light_brush, dark_brush, gray_brush]
+	for i in range(3):
+		imglist[i].resize(blend_rect_size, blend_rect_size)
+		imglist[i].convert(Image.FORMAT_RGBAH)
 
-	blendRect = Rect2(Vector2.ZERO, lightImage.get_size())
 
 
 func update_fog(pos, erase: bool = false):
-	lightImage.resize(blendImageHeight, blendImageWidth)
-	darkImage.resize(blendImageHeight, blendImageWidth)
-	grayImage.resize(blendImageHeight, blendImageWidth)
-	offset = Vector2(blendImageWidth / 2, blendImageHeight / 2) - Vector2(imgWidth / 2, imgHeight / 2)
-	blendRect = Rect2(Vector2.ZERO, lightImage.get_size())
-
+	var offset = Vector2(blend_rect_size / 2, blend_rect_size / 2) - Vector2(img_width / 2, img_height / 2)
+	var blend_rect = Rect2(Vector2.ZERO, Vector2.ONE * blend_rect_size)
 	if not erase:
-		fogImage.blend_rect(lightImage, blendRect, pos - offset)
-		fogTexture.update(fogImage)
+		dm_fog_image.blend_rect(light_brush, blend_rect, pos - offset)
+		dm_fog_texture.update(dm_fog_image)
 
-		fogImage2.blend_rect(lightImage, blendRect, pos - offset)
-		fogTexture2.update(fogImage2)
+		player_fog_image.blend_rect(light_brush, blend_rect, pos - offset)
+		player_fog_texture.update(player_fog_image)
 
 	if erase:
-		fogImage.blend_rect(grayImage, blendRect, pos - offset)
-		fogTexture.update(fogImage)
+		dm_fog_image.blend_rect(gray_brush, blend_rect, pos - offset)
+		dm_fog_texture.update(dm_fog_image)
 
-		fogImage2.blend_rect(darkImage, blendRect, pos - offset)
-		fogTexture2.update(fogImage2)
+		player_fog_image.blend_rect(dark_brush, blend_rect, pos - offset)
+		player_fog_texture.update(player_fog_image)
 
 
 func _process(_delta):
@@ -102,73 +106,155 @@ func _process(_delta):
 
 func _input(event: InputEvent) -> void:
 
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_C:
+			black_circle_bool = not black_circle_bool
+			settings_menu.set_item_checked(0, black_circle_bool)
+
 	if event.is_action_pressed('mod'):
-		modHeld = true
+		mod_held = true
 
 	if event.is_action_released('mod'):
-		modHeld = false
+		mod_held = false
 
-	if event.is_action_pressed('zoom_in') and modHeld:
-		blendImageWidth = max(5, blendImageWidth - 5)
-		blendImageHeight = max(5, blendImageHeight - 5)
+	if event.is_action_pressed('zoom_in') and mod_held:
+		update_brushes(-5)
 
-	if event.is_action_pressed('zoom_out') and modHeld:
-		blendImageWidth += 5
-		blendImageHeight += 5
+	if event.is_action_pressed('zoom_out') and mod_held:
+		update_brushes(5)
 
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			pressed = event.pressed
+		if hovering_over_gui:
+			return
 
-			if pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			m1_pressed = event.pressed
+
+			if m1_pressed:
 				update_fog(get_global_mouse_position())
 
 		if event.button_index == MOUSE_BUTTON_RIGHT:
-			pressed2 = event.pressed
+			m2_pressed = event.pressed
 
-			if pressed:
+			if m2_pressed:
 				update_fog(get_global_mouse_position(), true)
 
 	elif event is InputEventMouseMotion:
-		if pressed:
+		if m1_pressed:
 			update_fog(get_global_mouse_position())
-		elif pressed2:
+		elif m2_pressed:
 			update_fog(get_global_mouse_position(), true)
 
 func _draw() -> void:
-	draw_arc(get_global_mouse_position(), blendImageWidth * 0.5, 0.0, 2 * 3.141592, 100, Color.WHITE)
+	var circle_color : Color
+	if black_circle_bool:
+		circle_color = Color.BLACK
+	else:
+		circle_color = Color.WHITE
+
+	# draw_arc(get_global_mouse_position(), blend_rect_size * 0.5, 0.0, 2 * 3.141592, 100, circle_color, 1)
+	draw_circle(get_global_mouse_position(), blend_rect_size * 0.5, circle_color, false, 1)
+	print(get_global_mouse_position())
 
 func _on_file_id_pressed(id: int) -> void:
 	if id == 0:
-		fileDialog.popup()
+		file_dialog.popup()
+
+	if id == 1:
+		save_dialog.popup()
+
+	if id == 2:
+		get_tree().quit()
+
+func _on_settings_id_pressed(id: int) -> void:
+	if id == 0:
+		black_circle_bool = not black_circle_bool
+		settings_menu.set_item_checked(id, black_circle_bool)
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	load_map(path)
 
 
-func _on_file_dialog_file_selected(path:String) -> void:
-	var image = Image.new()
-	image.load(path)
+func load_map(path: String) -> void:
+	if path.ends_with(".map"):
+		var reader = ZIPReader.new()
+		reader.open(path)
+		dm_fog_image = Image.new()
+		dm_fog_image.load_png_from_buffer(reader.read_file("fog1.png"))
+		dm_fog_image.convert(Image.FORMAT_RGBAH)
 
-	imgWidth = image.get_size()[0]
-	imgHeight = image.get_size()[1]
+		player_fog_image = Image.new()
+		player_fog_image.load_png_from_buffer(reader.read_file("fog2.png"))
+		player_fog_image.convert(Image.FORMAT_RGBAH)
 
-	fogImage = Image.create(imgWidth, imgHeight, false, Image.FORMAT_RGBAH)
-	fogImage.fill(Color(0.5, 0.5, 0.5, 1))
-	fogTexture = ImageTexture.create_from_image(fogImage)
-	fog.texture = fogTexture
+		image = Image.new()
+		image.load_png_from_buffer(reader.read_file("image.png"))
+		image.convert(Image.FORMAT_RGBAH)
+
+		img_width = image.get_size()[0]
+		img_height = image.get_size()[1]
+
+		reader.close()
+
+	else:
+		image = Image.new()
+		image.load(path)
+		image.convert(Image.FORMAT_RGBAH)
+
+		img_width = image.get_size()[0]
+		img_height = image.get_size()[1]
 
 
+		dm_fog_image = Image.create(img_width, img_height, false, Image.FORMAT_RGBAH)
+		dm_fog_image.fill(Color(0.5, 0.5, 0.5, 1))
 
-	offset = Vector2(blendImageWidth / 2, blendImageHeight / 2) - Vector2(imgWidth / 2, imgHeight / 2)
+		player_fog_image = Image.create(img_width, img_height, false, Image.FORMAT_RGBAH)
+		player_fog_image.fill(Color(0, 0, 0, 1))
+
+	dm_fog_texture = ImageTexture.create_from_image(dm_fog_image)
+	dm_fog.texture = dm_fog_texture
+
+	player_fog_texture = ImageTexture.create_from_image(player_fog_image)
+	player_fog.texture = player_fog_texture
+
 
 	var image_texture = ImageTexture.new()
 	image_texture.set_image(image)
 
 	background.texture = image_texture
-	backgroundNode.position = -Vector2(imgWidth / 2, imgHeight / 2)
+	background_node.position = -Vector2(img_width / 2, img_height / 2)
 
 	# second window stuff
-	$PlayerWindow/PlayerRoot.add_child(backgroundNode.duplicate())
+	player_window.add_child(background_node.duplicate())
 
-	fogImage2 = Image.create(imgWidth, imgHeight, false, Image.FORMAT_RGBAH)
-	fogImage2.fill(Color(0, 0, 0, 1))
-	fogTexture2 = ImageTexture.create_from_image(fogImage2)
-	fog2.texture = fogTexture2
+
+func _on_file_dialog_2_file_selected(path:String) -> void:
+	var writer = ZIPPacker.new()
+	writer.open(path)
+	writer.start_file("fog1.png")
+	writer.write_file(dm_fog_image.save_png_to_buffer())
+	writer.start_file("fog2.png")
+	writer.write_file(player_fog_image.save_png_to_buffer())
+	writer.start_file("image.png")
+	writer.write_file(image.save_png_to_buffer())
+	writer.close_file()
+
+	writer.close()
+
+func _on_menu_bar_mouse_entered() -> void:
+	hovering_over_gui = true
+
+func _on_menu_bar_mouse_exited() -> void:
+	hovering_over_gui = false
+
+func _on_file_mouse_entered() -> void:
+	hovering_over_gui = true
+
+func _on_file_mouse_exited() -> void:
+	hovering_over_gui = false
+
+func _on_settings_mouse_entered() -> void:
+	hovering_over_gui = true
+
+func _on_settings_mouse_exited() -> void:
+	hovering_over_gui = false
