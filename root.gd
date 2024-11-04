48704c3c652131323e4a0df59b4a182fb143e1c3
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2024-11-04 10:01:29 +0100

    Finish proof of concept

--- root.gd
+++ root.gd
@@ -7,13 +7,17 @@ extends Control
 @onready var camera: Camera2D = $Camera2D
 @onready var playerWindow: Window = $PlayerWindow
 @onready var fog = $Fog
-@onready var background = $Background
+@onready var backgroundNode = $BackgroundNode
+@onready var background = $BackgroundNode/Background
 
 const LightTexture = preload('res://Light.png')
 const DarkTexture = preload('res://Dark.png')
 
 const lightHeight : int = 50
 const lightWidth : int = 50
+
+var imgHeight : int
+var imgWidth : int
 
 const fogHeight : int = 800
 const fogWidth : int = 800
@@ -38,71 +42,76 @@ var COLOR = Color.BLUE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    fileMenu.connect('id_pressed', _on_file_id_pressed)
-    fileDialog.add_filter("*.png, *.jpg, *.jpeg", "Images")
-    fileDialog.popup()
-    playerWindow.world_2d = get_window().world_2d
+	fileMenu.connect('id_pressed', _on_file_id_pressed)
+	fileDialog.add_filter("*.png, *.jpg, *.jpeg", "Images")
+	fileDialog.popup()
+	playerWindow.world_2d = get_window().world_2d
 
 
-    lightImage = LightTexture.get_image()
-    lightImage.resize(lightHeight, lightWidth)
-    lightImage.convert(Image.FORMAT_RGBAH)
+	lightImage = LightTexture.get_image()
+	lightImage.resize(lightHeight, lightWidth)
+	lightImage.convert(Image.FORMAT_RGBAH)
 
-    darkImage = DarkTexture.get_image()
-    darkImage.resize(lightHeight, lightWidth)
-    darkImage.convert(Image.FORMAT_RGBAH)
+	darkImage = DarkTexture.get_image()
+	darkImage.resize(lightHeight, lightWidth)
+	darkImage.convert(Image.FORMAT_RGBAH)
 
-    lightRect = Rect2(Vector2.ZERO, lightImage.get_size())
+	lightRect = Rect2(Vector2.ZERO, lightImage.get_size())
 
-    offset = Vector2(lightWidth / 2, lightHeight / 2) + Vector2(- fogWidth / 2, - fogHeight / 2)
 
 func update_fog(pos, image):
-    fogImage.blend_rect(image, lightRect, pos - offset)
-    fogTexture.update(fogImage)
+	fogImage.blend_rect(image, lightRect, pos - offset)
+	fogTexture.update(fogImage)
 
 
 func _process(_delta):
-    if Input.is_action_pressed("quit"):
-        get_tree().quit()
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
 
 func _input(event: InputEvent) -> void:
 
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT:
-            pressed = event.pressed
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			pressed = event.pressed
 
-            if pressed:
-                update_fog(get_global_mouse_position(), lightImage)
+			if pressed:
+				update_fog(get_global_mouse_position(), lightImage)
 
-        if event.button_index == MOUSE_BUTTON_RIGHT:
-            pressed2 = event.pressed
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			pressed2 = event.pressed
 
-            if pressed:
-                update_fog(get_global_mouse_position(), darkImage)
+			if pressed:
+				update_fog(get_global_mouse_position(), darkImage)
 
-    elif event is InputEventMouseMotion:
-        if pressed:
-            update_fog(get_global_mouse_position(), lightImage)
-        if pressed2:
-            update_fog(get_global_mouse_position(), darkImage)
+	elif event is InputEventMouseMotion:
+		if pressed:
+			update_fog(get_global_mouse_position(), lightImage)
+		if pressed2:
+			update_fog(get_global_mouse_position(), darkImage)
 
 
 func _on_file_id_pressed(id: int) -> void:
-    if id == 0:
-        fileDialog.popup()
+	if id == 0:
+		fileDialog.popup()
 
 
 func _on_file_dialog_file_selected(path:String) -> void:
-    var image = Image.new()
-    image.load(path)
+	var image = Image.new()
+	image.load(path)
 
-    fogImage = Image.create(image.get_size()[0], image.get_size()[1], false, Image.FORMAT_RGBAH)
-    fogImage.fill(Color.BLACK)
-    fogTexture = ImageTexture.create_from_image(fogImage)
-    fog.texture = fogTexture
+	fogImage = Image.create(image.get_size()[0], image.get_size()[1], false, Image.FORMAT_RGBAH)
+	fogImage.fill(Color.BLACK)
+	fogTexture = ImageTexture.create_from_image(fogImage)
+	fog.texture = fogTexture
 
-    var image_texture = ImageTexture.new()
-    image_texture.set_image(image)
+	imgWidth = image.get_size()[0]
+	imgHeight = image.get_size()[1]
 
-    background.texture = image_texture
-    fog.pos -= image.get_size()
+	offset = Vector2(lightWidth / 2, lightHeight / 2) - Vector2(imgWidth / 2, imgHeight / 2)
+
+	var image_texture = ImageTexture.new()
+	image_texture.set_image(image)
+
+	background.texture = image_texture
+	backgroundNode.position -= Vector2(imgWidth / 2, imgHeight / 2)
+	# fog.pos -= image.get_size()
